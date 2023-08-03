a15eb38be4dfed7d6248d49f9b623df159403279
Daniel Flø <Daniel.Flo@vkm.no>
2023-08-03 09:16:39 +0200

    first commit folder structure

---
+++ VKM_recommended_folder_structure.R
@@ -0,0 +1,40 @@
+
+vkm_data <- function(input = "vkm_data", is.SLR = FALSE, search.num = 1) {
+  newdir <- paste0(getwd(), "/", input, format(Sys.Date(), "_%Y.%m.%d"))
+  
+  # Check if directory already exists
+  if (!file.exists(newdir)) {
+    dir.create(newdir)
+    
+    # Create subdirectories
+    dir.create(paste0(getwd(), "/Data"), showWarnings = FALSE)
+    dir.create(paste0(getwd(), "/Literature_Data"), showWarnings = FALSE)
+    
+    for (s in 1:search.num) {
+      dir.create(paste0(getwd(), "/Literature_Data/search_", s), showWarnings = FALSE)
+      dir.create(paste0(getwd(), "/Literature_Data/search_", s, "/1_search_query"), showWarnings = FALSE)
+      dir.create(paste0(getwd(), "/Literature_Data/search_", s, "/2_study_selection"), showWarnings = FALSE)
+      dir.create(paste0(getwd(), "/Literature_Data/search_", s, "/3_full_text"), showWarnings = FALSE)
+      dir.create(paste0(getwd(), "/Literature_Data/search_", s, "/4_data_extraction"), showWarnings = FALSE)
+      
+      if (is.SLR) {
+        dir.create(paste0(getwd(), "/Literature_Data/search_", s, "/5_internal_validity"), showWarnings = FALSE)
+        dir.create(paste0(getwd(), "/Literature_Data/search_", s, "/6_confidence_in_evidence"), showWarnings = FALSE)
+      }
+    }
+    
+    dir.create(paste0(getwd(), "/Data/1_raw_data"), showWarnings = FALSE)
+    dir.create(paste0(getwd(), "/Data/2_processed_data"), showWarnings = FALSE)
+    dir.create(paste0(getwd(), "/Data/3_metadata"), showWarnings = FALSE)
+    dir.create(paste0(getwd(), "/Data/4_scripts"), showWarnings = FALSE)
+    dir.create(paste0(getwd(), "/Data/5_outputs"), showWarnings = FALSE)
+  } else {
+    print("Folder already exists.")
+  }
+}
+
+# kjør funksjon uten SLR 
+vkm_data("navn_paa_prosjekt")
+
+# SLR eksempel med fire søk
+vkm_data("SLR_prosjekt", is.SLR=TRUE, 4)

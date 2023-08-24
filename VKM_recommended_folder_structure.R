8f10603d61b975177b1a1ec32d341a7cce439598
Matthew Grainger <DrMattG@users.noreply.github.com>
2023-08-24 14:45:17 +0200

    suggest here::here as alternative to setwd/getwd()

--- VKM_recommended_folder_structure.R
+++ VKM_recommended_folder_structure.R
@@ -1,33 +1,39 @@
 
 vkm_data <- function(input = "vkm_data", is.SLR = FALSE, search.num = 1) {
-  newdir <- paste0(getwd(), "/", input, format(Sys.Date(), "_%Y.%m.%d"))
+  # check if here package is installed on users local system
+  list.of.packages <- c("here")
+  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+    # if not installed install it
+  if(length(new.packages)) install.packages(new.packages)
+  
+  newdir <- paste0(here::here(), "/", input, format(Sys.Date(), "_%Y.%m.%d"))
   
   # Check if directory already exists
   if (!file.exists(newdir)) {
     dir.create(newdir)
     
     # Create subdirectories
-    dir.create(paste0(getwd(), "/Data"), showWarnings = FALSE)
-    dir.create(paste0(getwd(), "/Literature_Data"), showWarnings = FALSE)
+    dir.create(paste0(here::here(), "/Data"), showWarnings = FALSE)
+    dir.create(paste0(here::here(), "/Literature_Data"), showWarnings = FALSE)
     
     for (s in 1:search.num) {
-      dir.create(paste0(getwd(), "/Literature_Data/search_", s), showWarnings = FALSE)
-      dir.create(paste0(getwd(), "/Literature_Data/search_", s, "/1_search_query"), showWarnings = FALSE)
-      dir.create(paste0(getwd(), "/Literature_Data/search_", s, "/2_study_selection"), showWarnings = FALSE)
-      dir.create(paste0(getwd(), "/Literature_Data/search_", s, "/3_full_text"), showWarnings = FALSE)
-      dir.create(paste0(getwd(), "/Literature_Data/search_", s, "/4_data_extraction"), showWarnings = FALSE)
+      dir.create(paste0(here::here(), "/Literature_Data/search_", s), showWarnings = FALSE)
+      dir.create(paste0(here::here(), "/Literature_Data/search_", s, "/1_search_query"), showWarnings = FALSE)
+      dir.create(paste0(here::here(), "/Literature_Data/search_", s, "/2_study_selection"), showWarnings = FALSE)
+      dir.create(paste0(here::here(), "/Literature_Data/search_", s, "/3_full_text"), showWarnings = FALSE)
+      dir.create(paste0(here::here(), "/Literature_Data/search_", s, "/4_data_extraction"), showWarnings = FALSE)
       
       if (is.SLR) {
-        dir.create(paste0(getwd(), "/Literature_Data/search_", s, "/5_internal_validity"), showWarnings = FALSE)
-        dir.create(paste0(getwd(), "/Literature_Data/search_", s, "/6_confidence_in_evidence"), showWarnings = FALSE)
+        dir.create(paste0(here::here(), "/Literature_Data/search_", s, "/5_internal_validity"), showWarnings = FALSE)
+        dir.create(paste0(here::here(), "/Literature_Data/search_", s, "/6_confidence_in_evidence"), showWarnings = FALSE)
       }
     }
     
-    dir.create(paste0(getwd(), "/Data/1_raw_data"), showWarnings = FALSE)
-    dir.create(paste0(getwd(), "/Data/2_processed_data"), showWarnings = FALSE)
-    dir.create(paste0(getwd(), "/Data/3_metadata"), showWarnings = FALSE)
-    dir.create(paste0(getwd(), "/Data/4_scripts"), showWarnings = FALSE)
-    dir.create(paste0(getwd(), "/Data/5_outputs"), showWarnings = FALSE)
+    dir.create(paste0(here::here(), "/Data/1_raw_data"), showWarnings = FALSE)
+    dir.create(paste0(here::here(), "/Data/2_processed_data"), showWarnings = FALSE)
+    dir.create(paste0(here::here(), "/Data/3_metadata"), showWarnings = FALSE)
+    dir.create(paste0(here::here(), "/Data/4_scripts"), showWarnings = FALSE)
+    dir.create(paste0(here::here(), "/Data/5_outputs"), showWarnings = FALSE)
   } else {
     print("Folder already exists.")
   }
